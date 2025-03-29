e6b839dcb450897c3c72f57055673af2feac823d
Naaham Shah <197281044+GrapheneScar@users.noreply.github.com>
2025-03-29 14:17:12 +0530

    Update committees-data.ts

--- src/components/committees/committees-data.ts
+++ src/components/committees/committees-data.ts
@@ -15,13 +15,13 @@ export const committeesData: Committee[] = [
       {
         name: "Bharatvir Saran",
         title: "Director",
-        photo: "/lovable-uploads/bharatvir.jpg",
+        photo: "/lovable-uploads/Bharatvir.png",
         bio: "Hey Delegates, Welcome to DISEC, I'm honoured to be your Director for JBCN Middle School MUN, I'm here to ensure that this is gonna be fun for you all while also introducing you all to the concept of MUNs. Doing MUN speaks a lot about a person, showing that they're capable enough to speak and discuss in front of others, present their viewpoints and most importantly have enough self confidence, don't let this intimidate you, MUN can also be your stepping stone to discovering skills you never knew you had. I believe in strong speakers who can defend their perspectives and challenge others with strong arguments. There is nothing like a bad viewpoint, so the delegate should be apt at providing justifiable statements to support themselves. Nothing can match the ecstasy of delivering a powerful speech, or submitting that one radical communique to change the committee. If I had one piece of advice for delegates it would be this: Just have fun. I'm Bharatvir Saran, Directing DISEC for JBCN Middle School MUN 2025, I'm looking forward to seeing you delegates in person. Regards from the JBCN MUN Team."
       },
       {
         name: "Ansh Shah",
         title: "Director",
-        photo: "/lovable-uploads/ansh.png",
+        photo: "/lovable-uploads/Ansh.png",
         bio: "Hello delegates, Welcome to DISEC at JBCN MS MUN where diplomacy is the real thing. I'm Ansh Shah, your Co-Director, and if there's one thing I love more than Man City and the Indian Cricket team, it's a debate so intense that it could cause an international crisis.\nThis year, we wonder whether Private Military Organizations are a threat to mankind or not? Are they mercenaries to hire, or the heroes for our peace that we have? You decide! Just remember, every organisation has its secrets, and I love it when delegates dig up classified dirt.\nI'm more than excited to help guide you through this deeply interesting topic and to unravel the mysteries behind Private military organisations. With 11 MUNs and this being my second time leading DISEC, I promise a committee full of chaos and hard-hitting diplomacy.\nIn my free time, you can catch me watching a cricket highlight, or trying to perfect my blazer most of the time. And yes, I do love playing cricket!\nI know it feels pretty overwhelming to begin MUN. Trust me, I've been there. It's about learning, engaging in debates, and finally, having some fun. So let loose a little from the barricades of your comfort zone and let's make this a fantastic experience together.\nLooking forward to a great conference!\nBest regards,\nAnsh Shah"
       }
     ],
@@ -41,13 +41,13 @@ export const committeesData: Committee[] = [
       {
         name: "Shaurya Kaushik",
         title: "Director",
-        photo: "/lovable-uploads/shaurya.png",
+        photo: "/lovable-uploads/Shaurya.png",
         bio: "Greetings delegates, I'm Shaurya and I am delighted to be your director for UNCOPUOS alongside my equally talented Assistant director Geiv. I am an avid MUNNER and it has been my passion ever since my first MUN in grade 7. I strongly believe that MUN is a fantastic platform to boost your confidence and public speaking skills. My advice for the first timers? Be confident, Confidence is always KEY. Although do not hesitate to approach me for any help, I'd be delighted to guide you through the intricacies of MUN procedure and speech making.\n\nApart from MUN, I love to play tennis and would be pleased to discuss anything about Formula 1 outside of committee. In my free time I usually busy myself with a good book or watch some of my favorite sitcoms.\n\nI strongly believe that COPUOS will be the most exciting committee in this year's conference. I look forward to seeing blistering, on-the spot-speeches, controversial communiques, unique solutions and detailed paperwork. I eagerly look forward to meeting you. Let's make COPUOS the best committee in JBCN middle school MUN!!\nRegards\nShaurya"
       },
       {
         name: "Geiv Mirza",
         title: "Assistant Director",
-        photo: "/lovable-uploads/geiv.jpeg",
+        photo: "/lovable-uploads/Geiv.png",
         bio: "Greetings delegates, I'm Geiv Mirza and I will be your assistant director for COPUOS alongside my director, Shaurya. We are ecstatic to navigate delegates through this unique agenda. Our committee will have a strong emphasis on problem-solving, diplomacy, the intricacies of outer-space exploration and powerful crises to keep you thinking on your feet. I can guarantee an action-packed, fast-paced and one-of-a-kind experience. I look forward to seeing controversial speeches, long POIs and logical solutions from the delegates present.\nFor all the first timers, I encourage you to actively engage in debate and most importantly be confident. This conference is a good experience; You may arrive expecting nothing but leave as a best delegate.\nDuring my spare time I love to spectate and commentate on combat sports, listen to hip-hop, play video games, video edit and code. Within academics, I am fond of computer science, physics and economics."
       }
     ],
@@ -67,13 +67,13 @@ export const committeesData: Committee[] = [
       {
         name: "Kasmia Mehra",
         title: "Director",
-        photo: "/lovable-uploads/kasmia.jpg",
+        photo: "/lovable-uploads/Kasmia.png",
         bio: "Hello Delegates! I'm Kasmia Mehra, Director of the Korean War Summit and I can't wait for this amazing committee to begin! :)\n\nI did my first MUN in 6th grade and haven't stopped since then. I truly feel it is the epitome of human society when people can come together despite difficult situations and find solutions for their problems together. This paired with the facts that MUN allows you to explore different perspectives of the world with a fresh perspective just adds to my love for it.\n\nBut MUN aside I am an avid sculpture artist who enjoys swimming in her free time. My favorite thing to do in the world is to see it, traveling. I think seeing, hearing, learning something new is the greatest feeling in the world. With that I hope to learn many new things from the Delegates who sign up for the K.W.S."
       },
       {
         name: "Adhiraj Bhatia",
         title: "Assistant Director",
-        photo: "/lovable-uploads/adhiraj.jpeg",
+        photo: "/lovable-uploads/Adhiraj.png",
         bio: "I'm Adhiraj Bhatia, and I'm thrilled to be chairing the Korean War Summit at JBCN Middle School MUN 2025!\nFrom online to abroad conferences, MUN has become an integral part of my school life. Beyond MUN, I'm a huge Brooklyn Nine-Nine and Cobra Kai fanatic—so as you can probably tell, I absolutely love to Netflix and chill. I'm deeply passionate about global politics and debate, always ready for insightful discussions.\nAnother major part of my life? Eating oats(while Netflix and chilling). I strongly believe they are one of life's greatest pleasures. And if you disagree, I'm open to a brutal argument about that too—not joking. Also, not a threat. Perhaps my tenacious approach is kicking in!\nI can't wait to see all of you in the Korean War Summit, alongside my fellow Executive Board and the organizing team, for what promises to be an exciting, intense, and meaningful experience!"
       }
     ],
@@ -93,13 +93,13 @@ export const committeesData: Committee[] = [
       {
         name: "Dishita Sehgal",
         title: "Director",
-        photo: "/lovable-uploads/dishita.png",
+        photo: "/lovable-uploads/Dishita.png",
         bio: "Hello Delegates! I am Dishita Sehgal, your Director for the committee Cabinet of India and I can't wait to see the energy, passion and unique perspectives you all will bring to this conference.\n\nFrom my first MUN to now I have always believed that MUN isn't just about debating and finding solutions, it's about making strategies, negotiations and most importantly stepping out of your comfort zone. If there is one thing I love, it's a high-energy environment where every decision counts, whether it's a film set or a debate. For those who are stepping into MUN for the very first time, I know the mix of nerves and excitement you all must be feeling as I felt the same during my first middle school MUN in 7th grade but trust me once you embrace the challenge there is nothing quite like it, MUN's are more than just awards it's about learning, understanding and finding your own voice in diplomacy.\nA bit about myself is that acting, debating, and dancing have always been my passions. From working in films like Hindi Medium, War to having my art featured in Hindustan Times I have explored the art of storytelling in many forms. Stepping into an historical cabinet is like stepping into a political thriller, being a part of this cabinet means that your every decision will shape the direction in which the committee goes. Operation Blue star wasn't just any ordinary operation it was one which shaped the Indian history. So get ready for an unforgettable journey filled with intense debates, unexpected and twist and last but not the least I hope you all have a great time.\nRegards,\nDishita"
       },
       {
         name: "Pratham Nigotiya",
         title: "Assistant Director",
-        photo: "/lovable-uploads/pratham.jpg",
+        photo: "/lovable-uploadsPratham.png",
         bio: "Namaste Delegates! Being the assistant director of this Cabinet of India- discussing the infamous Operation Bluestar, is something that I always longed to do.\n\nApart from supporting the BJP and highlighting what all India's best PM has done, I love reading different kinds of books and writing my thoughts in the form of poetry. I love debating, and aspire to practice law. I'm very approachable for almost everything, so don't give a second thought if you want to ask me something, clarify something or just have fun with me.\n\nWhen I had done my first ever MUN, I did not care about what people would think of me. (that's because no one really knew me) I advise you to do the same, and debate your heart out. This committee will be one of the most entertaining ones you've ever been a part of."
       }
     ],
@@ -119,13 +119,13 @@ export const committeesData: Committee[] = [
       {
         name: "Arjun Asher",
         title: "Director",
-        photo: "/lovable-uploads/arjun.jpeg",
+        photo: "/lovable-uploads/Arjun.png",
         bio: "Hi I'm Arjun Asher, a 14-year-old student who also has a passion for debates, public speaking and of-course MUN. 3 years ago, I attended my 1st Model United Nations, in this very conference, only difference was back I was a delegate but now I have stepped up being the director of the Russian Federative Assembly, so buckle up and get ready for this conference coming your way.\nBeyond MUN, I am big on Geo-Politics, and easily fascinated by world events. Other than averaging over a thousand minutes monthly on Spotify listening to Kendrick Lamar, I have a soft spot for collecting sneakers and fragrances. Aside from listening to music I have also played the piano for around 6 years now. I keep my tabs on football especially 'The Reds' and do enjoy playing basketball on a daily basis. In school I am a passionate learner, ready to tackle any challenge and hopefully help my fellow delegates and guide them in the right direction."
       },
       {
         name: "Niaan Dand",
         title: "Assistant Director",
-        photo: "/lovable-uploads/niaan.jpeg",
+        photo: "/lovable-uploads/Niaan.png",
         bio: "I'm Niaan, the assistant director of the Russian Federative Assembly alongside Arjun. We promise an exhilarating experience for all delegates.\n\nFrom having attended a plethora of conferences both as a delegate and EB member, I've seen it all, and truly understand all types of delegates even if they are first timers. I encourage all delegates to speak up, not be scared and be confident in the committee.\n\nFrom being the biggest fan of Donald Trump and a world rank player in Call of Duty Black ops, you can talk to me for hours about F1, politics and of course, Suits(Netflix). When I'm not debating in the committee room, you might catch me playing Call of Duty, listening to music, playing the drums, or studying for an upcoming test.\n\nBeing the only constant crisis committee, the Russian Federative Assembly is not going to be less than a roller coaster ride where major twists and turns will depict the future of the Assembly.\n\nAs I bring my inner Harvey Specter, Mike Ross and Neal Caffrey out, I am looking forward to interacting with all delegates.\n\nRock it!\n\nNiaan Dand."
       }
     ],
