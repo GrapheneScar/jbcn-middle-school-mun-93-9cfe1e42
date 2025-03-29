fa48d514c6c6b03d848c2261ec90fd3ada72ad41
Naaham Shah <197281044+GrapheneScar@users.noreply.github.com>
2025-03-29 16:08:54 +0530

    Update ExamplesSection.tsx

--- src/components/resources/ExamplesSection.tsx
+++ src/components/resources/ExamplesSection.tsx
@@ -8,38 +8,38 @@ const templateResources = [{
   description: "Example for writing effective directives.",
   icon: FilePenLine,
   link: "https://docs.google.com/document/d/17fUQgZ_lIoubRNQcwFo6YD-LKUnHjt2cNWuUxFYw8vg/edit?usp=sharing",
-  image: "/lovable-uploads/",
+  image: "/lovable-uploads/Directive.png",
   highlight: false
 }, {
   title: "Draft Resolution",
   description: "Example for writng a excellent Draft Resolution.",
   icon: FileText,
   link: "https://docs.google.com/document/d/11PPDUUnyy1VT29eCLhsvEicsIZ3PtjEg/edit?usp=sharing&ouid=100119734129423747207&rtpof=true&sd=true",
-  image: "/lovable-uploads/"
+  image: "/lovable-uploads/Draft resolution.png"
 }, {
   title: "Position Paper",
   description: "Example for crafting effective position papers",
   icon: FileCode,
   link: "https://docs.google.com/document/d/138mRyGFn9Tr2231kekwqnWqmm7ahtQvkjsPuSdEWoRM/edit?usp=sharing",
-  image: "/lovable-uploads/"
+  image: "/lovable-uploads/Position paper.png"
 }, {
   title: "communiqué",
   description: "Example for format and structure for working communiqué.",
   icon: FilePlus2,
   link: "https://drive.google.com/file/d/1MSwe-OaZGGLMpw2fde5Q9pIiZM2c9mGv/view?usp=sharing",
-  image: "/lovable-uploads/"
+  image: "/lovable-uploads/Communique.png"
 }, {
   title: "Press Release (communiqué)",
   description: "Example for drafting press releases (communiqué)",
   icon: Newspaper,
   link: "https://drive.google.com/file/d/19lOGxXn5B3ouaYN3mrNeBXBTY-oUTGsO/view?usp=sharing",
-  image: "/lovable-uploads/"
+  image: "/lovable-uploads/Press release.png"
 }, {
   title: "Chits",
   description: "Example for writing effective communication chits",
   icon: MessageSquare,
   link: "https://drive.google.com/file/d/1gp1suebS-P0O4Fo2pXNWS6k9TFbTOrBB/view?usp=sharing",
-  image: "/lovable-uploads/"
+  image: "/lovable-uploads/Chit.png"
 }];
 
 const ExamplesSection = () => {
