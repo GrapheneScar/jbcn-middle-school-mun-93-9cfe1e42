01ecdf5326910245b9b8cf49952494e99d9f269e
Naaham Shah <197281044+GrapheneScar@users.noreply.github.com>
2025-04-05 09:18:21 +0530

    Update committees-data.ts

--- src/components/committees/committees-data.ts
+++ src/components/committees/committees-data.ts
@@ -16,7 +16,7 @@ export const committeesData: Committee[] = [
     topics: ["Evaluating the prospect of Private Military Organizations"],
     description: "Greetings Delegates, Welcome to DISEC, where we all attempt to bring peace through our resolutions while the plethora of mercenaries break it. This year's agenda? Private Military Organisations (PMOs), the freelancers of war who just can't seem to get enough money. Are they really the security experts as we know them or international chaos machines exploiting conflict zones? Some argue they 'assist' in conflicts (get paid to persecute people living through war), while others say they're just capitalism's version of war, why employ soldiers when you can hire some on a paid subscription plan? Groups like Wagner, Blackwater, and Aegis Defence have turned global conflict into their bread and butter, often operating outside the law and making governments and sovereignty look like side characters. So, delegates, the burning question is: are PMOs necessary evils, or just evils with a booming business model? Get ready to debate, expose deals, and maybe find a common solution. So are you delegates ready? Because we are!",
     studyGuideUrl: "https://drive.google.com/file/d/1VxI8qSc5XYv-V0_JLETQVwE91DhCom_g/view?usp=sharing",
-    countryMatrixUrl: "https://drive.google.com/file/d/1VxI8qSc5XYv-V0_JLETQVwE91DhCom_g/view?usp=sharing",
+    countryMatrixUrl: "https://docs.google.com/spreadsheets/d/1AnkMh7dEaqQ7kXAUx2tWjrsTmaPbPAap6BoL2l9sMNQ/edit?usp=sharing",
     chairs: [
       {
         id: "disec-chair-1",
@@ -50,7 +50,7 @@ export const committeesData: Committee[] = [
     topics: ["Addressing the challenges and possibilities of weaponization in space"],
     description: "Greetings delegates, Welcome to the United Nations Committee on the Peaceful Uses of Outer Space. This committee serves as a vital platform for international cooperation in the exploration and utilization of outer space, ensuring that its benefits are accessible to all nations. Militarisation and weaponization in space is a new challenge presented to be tackled in a diplomatic manner considering the consequences and weight of every decision with constant twists and crises to maintain the pressure. This is a high-stakes scenario on the possible weaponization in space that must be looked at from each legal, social, economical, and logistical standpoint. The purpose of UNCOPUOS is to maintain outer space as a domain dedicated to peaceful purposes, promoting collaboration and addressing the challenges that arise from space activities. The potential deployment of weapons systems in outer space presents a grave and destabilizing threat to international peace and security. Such actions could escalate tensions between nations, transforming the vast expanse of space into a domain of conflict. Now it is up to you to navigate through this committee, resolving potential crises, launching satellites and preventing interstellar warfare to ensure peace and stability. Everything is on the line, the future of our world lies in your hands.",
     studyGuideUrl: "https://drive.google.com/file/d/1bCr3cf1AwbFNEBQOHCPZPUByjlP_gKut/view?usp=sharing",
-    countryMatrixUrl: "https://drive.google.com/file/d/1bCr3cf1AwbFNEBQOHCPZPUByjlP_gKut/view?usp=sharing",
+    countryMatrixUrl: "https://docs.google.com/spreadsheets/d/1AnkMh7dEaqQ7kXAUx2tWjrsTmaPbPAap6BoL2l9sMNQ/edit?usp=sharing",
     chairs: [
       {
         id: "copuos-chair-1",
@@ -84,7 +84,7 @@ export const committeesData: Committee[] = [
     topics: ["To negotiate lasting peace and end the Korean War, possibly uniting or dividing the peninsula"],
     description: "The Korean War Summit is set on 10 April 1952. Tensions are rising between the North and South. Every country is involved and everyone has a stake to lose. This is the forgotten war that changed our world into what it is today.With a death Toll of over one million and resources diminishing what is bound to happen? Will we be able attain peace or are more casualties imminent? The real question however is will you be able to change history and make it your own? This is the forgotten war that changed our world into what it is today or rather what you, the Delegates will shape it into. Coming to the reasons for having this committee was to bring a fulfilling and enjoyable experience. This being a middle school mun was the perfect opportunity for Delegates to to step out of their comfort zone while still having some help. And that's what KWS is all about. The committee is not only being fun and compelling but also gaining first hand experience, seeing events in actions and trying to the best possible Delegate you can be One of the main reasons for having this committee was to bring a fulfilling and enjoyable experience. This being a middle school mun was the perfect opportunity for Delegates to to step out of their comfort zone while still having some help. And that's what KWS is all about.  The committee is not only being fun and compelling but also gaining first hand experience, seeing events in actions. Winston Churchill once said, 'In the middle of every difficulty lies opportunity.' So while you try to find your way through the Korean war, this is also your chance to try to find the best possible Delegate you can be.",
     studyGuideUrl: "https://drive.google.com/file/d/1D5QtyBXHWxW2HxnDy2c_X7dYRLqNAQnC/view?usp=sharing",
-    countryMatrixUrl: "https://drive.google.com/file/d/1D5QtyBXHWxW2HxnDy2c_X7dYRLqNAQnC/view?usp=sharing",
+    countryMatrixUrl: "https://docs.google.com/spreadsheets/d/1AnkMh7dEaqQ7kXAUx2tWjrsTmaPbPAap6BoL2l9sMNQ/edit?usp=sharing",
     chairs: [
       {
         id: "kws-chair-1",
@@ -115,7 +115,7 @@ export const committeesData: Committee[] = [
     topics: ["Evaluating the significance and implications of Operation Blue Star"],
     description: "Namaste Delegates, The Cabinet of India was set on 3rd June 1984. 1984 is the year which defined India's destiny. The nation stands one decision away from stability and one misstep away from chaos. With Operation Blue Star underway, tensions are at their peak. The government faces mounting pressure as it navigates the complexities of national security, civil unrest, and political turmoil. The weight of the nation now rests on the shoulder of its leaders, every decision made today will shape history. Everything is at stake: public trust, political stability and the most important unity of India. This isn't just another history lesson, it's where the past is brought to life. This committee gives you the chance to experience how real governments make decisions in times of crisis. It's designed to introduce middle school delegates to regional bodies, where domestic policies, leadership struggles, and high-pressure decisions define the debate. Its also there to break the misconception that historical committees are rigid and predictable instead, they're dynamic, engaging, and full of unexpected twists.",
     studyGuideUrl: "https://drive.google.com/file/d/1PSQLp6D759fiIKIR8pWJdxFCuUxXlyNy/view?usp=sharing",
-    countryMatrixUrl: "https://drive.google.com/file/d/1PSQLp6D759fiIKIR8pWJdxFCuUxXlyNy/view?usp=sharing",
+    countryMatrixUrl: "https://docs.google.com/spreadsheets/d/1AnkMh7dEaqQ7kXAUx2tWjrsTmaPbPAap6BoL2l9sMNQ/edit?usp=sharing",
     chairs: [
       {
         id: "coi-chair-1",
@@ -146,7 +146,7 @@ export const committeesData: Committee[] = [
     topics: ["Examining and developing mechanisms to prevent mismanagement of funds and power within the Kremlin"],
     description: "The Russian Federative Assembly at JBCN Middle school MUN will be no less than a roller coaster ride for all delegates. The RFA is the only constant crisis committee in MSMUN, think of it as a high stakes political chess game where the rules keep changing. Tasked with ensuring transparency and accountability (which the Kremlin is not famous for), delegates are required to tackle corruption, economic mismanagement, and power hungry politicians while navigating sudden crisis updates. This committee's flow will shift with each crisis, forcing delegates to make quick decisions, adapt strategies, and maybe even pull off some backroom deals! This committee is all about survival, diplomacy and making sure Russia doesn't spiral into chaos. To all delegates, be prepared to dodge all the bullets that come towards you, because it is one heck of a ride! Best of luck! Arjun Asher and Niaan Dand",
     studyGuideUrl: "https://drive.google.com/file/d/16LJpHIDCcjDIHijqvKGRJPpjg7pFd5ae/view?usp=sharing",
-    countryMatrixUrl: "https://drive.google.com/file/d/16LJpHIDCcjDIHijqvKGRJPpjg7pFd5ae/view?usp=sharing",
+    countryMatrixUrl: "https://docs.google.com/spreadsheets/d/1AnkMh7dEaqQ7kXAUx2tWjrsTmaPbPAap6BoL2l9sMNQ/edit?usp=sharingg",
     chairs: [
       {
         id: "rfa-chair-1",
